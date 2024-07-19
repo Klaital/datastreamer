69ef969d671dc706d09b5bc89a4e2e5f4bcacc9c
Kit Cox <chriscox@abandonedfactory.net>
2024-07-19 15:30:48 -0700

    Adding row number to output

--- datastream.py
+++ datastream.py
@@ -17,23 +17,27 @@ class DataStreamer:
         self.db = dbconn
 
     def run_query(self, query, params):
+        # keep track of what row number we're processing overall
+        overall_rows_handled = 0
+
         # Run the query one row at a time
         # Track how many rows are processed, and bytes read, per second
         rows_handled = 0
         bytes_handled = 0
         start_time = datetime.datetime.now()    # Track how long the whole process took
         window_start = datetime.datetime.now()  # Track how many bytes/rows were processed in a single time interval
-        cursor = self.db.execute(query, params)
+        self.db.execute(query, params)
 
-        row = cursor.fetchone()
+        row = self.db.fetchone()
         while row is not None:
             row_end_time = datetime.datetime.now()
             rows_handled += 1
+            overall_rows_handled += 1
             bytes_handled += sys.getsizeof(row)
             if row_end_time - window_start > datetime.timedelta(seconds=1):
-                # \r causes this line to rewrite itself each time
                 # TODO: calculate bytes/s more precisely using the actual window duration rather than assuming exactly 1 second.
-                sys.stdout.write("\r{0} rows/s\t{1} bytes/s".format(rows_handled, convert_size(bytes_handled)))
+                # \r causes this line to rewrite itself each time
+                sys.stdout.write("\rline {0}: {1} rows/s\t{2} bytes/s".format(overall_rows_handled, rows_handled, convert_size(bytes_handled)))
                 # Reset the stats
                 rows_handled = 0
                 bytes_handled = 0
